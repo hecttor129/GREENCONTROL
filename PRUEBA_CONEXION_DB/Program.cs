2a6d55abd9d0a7affaf6cbde7eef57f9532dfe45
hecttor129 <hectorcarrillom12@gmail.com>
2025-11-12 23:46:19 -0500

    ACTUALIZACION CONTROLLER

--- PRUEBA_CONEXION_DB/Program.cs
+++ PRUEBA_CONEXION_DB/Program.cs
@@ -8,7 +8,70 @@ class Program
 {
     static void Main(string[] args)
     {
+        ProbarConexionProgram();
+    }
 
+
+
+
+
+
+
+//    void sistemariego()
+//    {
+//        Sensor sensor = new Sensor
+//        {
+//            Id = 1,
+//            IdParcela = 1,
+//            HumedadActual = 100,
+//            FrecuenciaLectura = 1 // cada 1 segundo
+//        };
+
+//        SimuladorRiego simulador = new SimuladorRiego
+//        {
+//            Id = 1,
+//            IdParcela = 1,
+//            EstadoRiego = "INACTIVO",
+//            UmbralActivacion = 90
+//        };
+
+//        SensorService sensorService = new SensorService();
+//        SimuladorRiegoService riegoService = new SimuladorRiegoService();
+
+//        System.Timers.Timer timer = new System.Timers.Timer(sensor.FrecuenciaLectura.Value * 1000);
+//        timer.Elapsed += (sender, e) =>
+//        {
+//            // Si el riego está inactivo, la humedad baja
+//            if (simulador.EstadoRiego == "INACTIVO")
+//            {
+//                sensorService.SimularDisminucion(sensor, 1);
+//            }
+//            else
+//            {
+//                // Si el riego está activo, sube gradualmente
+//                sensor.HumedadActual += 1;
+
+//                if (sensor.HumedadActual >= 100)
+//                {
+//                    sensor.HumedadActual = 100;
+//                    simulador.EstadoRiego = "INACTIVO";
+//                }
+//            }
+
+//            // Supervisar el estado del riego según la humedad
+//            riegoService.SupervisarSensor(sensor, simulador);
+
+//            Console.WriteLine($"[{DateTime.Now:T}] Parcela {sensor.IdParcela} → Humedad: {sensor.HumedadActual}% | Riego: {simulador.EstadoRiego}");
+//        };
+
+//        timer.Start();
+//        Console.WriteLine("Simulación de sensor iniciada. Presiona Enter para detener...");
+//        Console.ReadLine();
+//        timer.Stop();
+
+//}
+    static void ProbarConexionProgram()
+    {
         Console.WriteLine(" Probando conexión a Oracle...");
 
         ConexionOracle conexion = new ConexionOracle();
@@ -24,62 +87,6 @@ class Program
 
         Console.WriteLine("Presiona una tecla para salir...");
         Console.ReadKey();
-
-
-        void sistemariego()
-        {
-            //    Sensor sensor = new Sensor
-            //    {
-            //        Id = 1,
-            //        IdParcela = 1,
-            //        HumedadActual = 100,
-            //        FrecuenciaLectura = 1 // cada 1 segundo
-            //    };
-
-            //    SimuladorRiego simulador = new SimuladorRiego
-            //    {
-            //        Id = 1,
-            //        IdParcela = 1,
-            //        EstadoRiego = "INACTIVO",
-            //        UmbralActivacion = 90
-            //    };
-
-            //    SensorService sensorService = new SensorService();
-            //    SimuladorRiegoService riegoService = new SimuladorRiegoService();
-
-            //    System.Timers.Timer timer = new System.Timers.Timer(sensor.FrecuenciaLectura.Value * 1000);
-            //    timer.Elapsed += (sender, e) =>
-            //    {
-            //        // Si el riego está inactivo, la humedad baja
-            //        if (simulador.EstadoRiego == "INACTIVO")
-            //        {
-            //            sensorService.SimularDisminucion(sensor, 1);
-            //        }
-            //        else
-            //        {
-            //            // Si el riego está activo, sube gradualmente
-            //            sensor.HumedadActual += 1;
-
-            //            if (sensor.HumedadActual >= 100)
-            //            {
-            //                sensor.HumedadActual = 100;
-            //                simulador.EstadoRiego = "INACTIVO";
-            //            }
-            //        }
-
-            //        // Supervisar el estado del riego según la humedad
-            //        riegoService.SupervisarSensor(sensor, simulador);
-
-            //        Console.WriteLine($"[{DateTime.Now:T}] Parcela {sensor.IdParcela} → Humedad: {sensor.HumedadActual}% | Riego: {simulador.EstadoRiego}");
-            //    };
-
-            //    timer.Start();
-            //    Console.WriteLine("Simulación de sensor iniciada. Presiona Enter para detener...");
-            //    Console.ReadLine();
-            //    timer.Stop();
-            //}
-
-        }
 
     }
 }
